3271dbee122e1664b6407e7f2e9b7c4c4c75138f
Jeff Kirui <kipkoech_g@yahoo.com>
2023-11-16 14:29:58 +0300

    Fixing bugs

--- main_monty.c
+++ main_monty.c
@@ -3,6 +3,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+char **op_toks = NULL;
+
 /**
  * main - the entry point for Monty Interp
  * @argc: the count of arguments passed to the program
@@ -11,7 +13,6 @@
  */
 int main(int argc, char **argv)
 {
-	char **op_toks = NULL;
 	FILE *script_fd = NULL;
 	int exit_code = EXIT_SUCCESS;
 
